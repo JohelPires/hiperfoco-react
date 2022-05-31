8f8bf9280bbcc15cb664fcd26c804e2f44d02228
JohelPires <johelpires@gmail.com>
2022-05-31 18:42:32 -0400

    Pomodoro com os 4 ciclos completo. limpar codigo.

--- src/Pomodoro.js
+++ src/Pomodoro.js
@@ -5,21 +5,68 @@ function Pomodoro() {
 
     const [focoHoje, setFocoHoje] = React.useState({
         pomoCount: 0,
-        tempoPomoAtual: '25:00'
+        pomoTempo: 1500,
+        pausaCurta: 300,
+        pausaLonga: 900
     })
 
     const PomoTempo = 5 //  1500 = 25 minutos
 
     const [counter, setCounter] = React.useState(PomoTempo)
-    const [counterStatus, setCounterStatus] = React.useState(false)
+
+    // Counter Status:
+    // 'parado', 'pomo', 'pausaCurta', 'pausaLonga'
+    // pomoCount:
+    // 0 = parado
+    // 1 = Foco
+    // 2 = pausa curta
+    // 3 = Foco
+    // 4 = pausa curta
+    // 5 = Foco
+    // 6 = pausa curta
+    // 7 = Foco
+    // 8 = pausa longa
+
+    // TO-DO: 
+    // - stop button: sets pomoCount to 0, asks for confirmation
+
+
+    const [counterStatus, setCounterStatus] = React.useState('parado')
+    const [counterPlayPause, setCounterPlayPause] = React.useState(false)
+    const [pomoCount, setPomoCount] = React.useState(0)
+    
 
     let intervalID
 
     React.useEffect(() => {
         const timer = 
-            counterStatus && setInterval(() => setCounter(counter - 1), 1000);
+            counterPlayPause && setInterval(() => setCounter(counter - 1), 1000);
 
         if (counter === 0){
+            // PLAY SOUND, CHANGE BG COLOR, ETC.
+
+            
+            
+            
+            
+            setCounterStatus((prev) => {
+                if (pomoCount % 2 !== 0 && pomoCount < 7){
+                    setCounter(3) // mudar para pausa curta: setCounter(pausaCurta)
+                    return 'Pausa curta'
+                } else if (pomoCount % 2 === 0 && pomoCount < 7){
+                    setCounter(5) // Mudar para tempo de foco
+                    return 'Foco'
+                } else if (pomoCount === 7){
+                    setCounter(4) // Mudar para pausa longa
+                    return 'Pausa Longa'
+                } else if (pomoCount > 7) {
+                    setPomoCount(1)
+                    setCounter(5) //Mudar para tempo de foco
+                    return 'Foco'
+                }                
+            })
+
+            setPomoCount(prevPomoCount => prevPomoCount + 1)
 
             // set counter to the short pause value
         }
@@ -27,8 +74,14 @@ function Pomodoro() {
       }, [counter, counterStatus]);
 
     function toggle() {
-        setCounterStatus((prevStat) => !prevStat)
+        if (pomoCount === 0){
+            setPomoCount(1)
+            setCounterStatus('Foco')
+        } // Significa o início da contagem. Depois disso a contagem é gerenciada dentro do useEffect.
+        setCounterPlayPause((prevStat) => !prevStat)
     }
+
+    console.log(pomoCount)
 
     function minutos(sec){
 
@@ -49,14 +102,15 @@ function Pomodoro() {
 
     }
 
-
-    console.log(minutos(1500))
-    console.log(minutos(1499))
+    console.log(pomoCount)
+    // console.log(minutos(1500))
+    // console.log(minutos(1499))
 
   return (
     <div className='pomodoro'>
         <h1>{minutos(counter)}</h1>
-        <div onClick={toggle} className='play-pause'>{counterStatus ? <BsPauseCircle /> : <BsPlayCircle />}</div>
+        <h3>{counterStatus}</h3>
+        <div onClick={toggle} className='play-pause'>{counterPlayPause ? <BsPauseCircle /> : <BsPlayCircle />}</div>
         {/* <div onClick={stop} className='play-pause'><BsPlayCircle /></div> */}
     </div>
   )
